18fee36f300c49e08befcd42b9720a5ec61eb2c3
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-06-09 23:32:17 +0300

    add responsive SpecialMenu section

--- src/constants/data.js
+++ src/constants/data.js
@@ -2,81 +2,74 @@ import images from './images';
 
 const wines = [
   {
-    title: 'Chapel Hill Shiraz',
-    price: '$56',
-    tags: 'AU | Bottle',
+    title: 'Rift Valley Syrah',
+    price: '$18.00',
+    tags: 'ET | Premium Dry Red',
   },
   {
-    title: 'Catena Malbee',
-    price: '$59',
-    tags: 'AU | Bottle',
+    title: 'Rift Valley Merlot',
+    price: '$18.00',
+    tags: 'ET | Premium Dry Red',
   },
   {
-    title: 'La Vieillw Rose',
-    price: '$44',
-    tags: 'FR | 750 ml',
+    title: 'Rift Valley Cabernet Sauvignon',
+    price: '$17.00',
+    tags: 'ET | Premium Dry Red',
   },
   {
-    title: 'Rhino Pale Ale',
-    price: '$31',
-    tags: 'CA | 750 ml',
+    title: 'Rift Valley Chardonnay',
+    price: '$18.00',
+    tags: 'ET | Premium Dry White',
   },
   {
-    title: 'Irish Guinness',
-    price: '$26',
-    tags: 'IE | 750 ml',
+    title: 'Acacia Medium Sweet Red',
+    price: '$16.00',
+    tags: 'ET | Semi-Sweet Red',
+  },
+  // {
+  //   title: 'Acacia Medium Sweet White',
+  //   price: '$16.00',
+  //   tags: 'ET | Semi-Sweet White',
+  // },
+  // {
+  //   title: 'Awash Mille Fleurs',
+  //   price: '$14.00',
+  //   tags: 'ET | Red Blend',
+  // },
+  // {
+  //   title: 'Awash Crystal',
+  //   price: '$14.00',
+  //   tags: 'ET | White',
+  // },
+];
+
+const beers = [
+  {
+    title: 'Habesha Gold',
+    price: '$2.50',
+    tags: 'Golden lager | Light body',
+  },
+  {
+    title: 'Bedele Special',
+    price: '$2',
+    tags: 'Pale lager | Strong bitterness',
+  },
+  {
+    title: 'Dashen Lager',
+    price: '$1.80',
+    tags: 'Classic lager | Ethiopian barley',
+  },
+  {
+    title: 'Harar Beer',
+    price: '$1.80',
+    tags: 'Pale lager| Refreshing',
+  },
+  {
+    title: 'Walia',
+    price: '$1.90',
+    tags: 'Light lager | Mild flavor | Easy drinking',
   },
 ];
 
-const cocktails = [
-  {
-    title: 'Aperol Sprtiz',
-    price: '$20',
-    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
-  },
-  {
-    title: "Dark 'N' Stormy",
-    price: '$16',
-    tags: 'Dark rum | Ginger beer | Slice of lime',
-  },
-  {
-    title: 'Daiquiri',
-    price: '$10',
-    tags: 'Rum | Citrus juice | Sugar',
-  },
-  {
-    title: 'Old Fashioned',
-    price: '$31',
-    tags: 'Bourbon | Brown sugar | Angostura Bitters',
-  },
-  {
-    title: 'Negroni',
-    price: '$26',
-    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
-  },
-];
 
-const awards = [
-  {
-    imgUrl: images.award02,
-    title: 'Bib Gourmond',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award01,
-    title: 'Rising Star',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award05,
-    title: 'AA Hospitality',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award03,
-    title: 'Outstanding Chef',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-];
-
-export default { wines, cocktails, awards };
+export default { wines, beers };
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 import './SpecialMenu.css';
 
-const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
-);
+
+function SpecialMenu() {
+  return (
+    <div className='app__specialMenu flex__center section__padding' id='menu'>
+      <div className="app__specilaMenu-title">
+        <SubHeading title="Discover flavors that fit your palate"/>
+        <h1 className="headtext__cormorant">Today's Special</h1>
+      </div>
+
+      <div className="app__special-menu">
+        <div className="app__specialMenu_wine flex__center">
+          <p className='app__specialMenu_heading'>Wines</p>
+          <div className="app__specialMenu-items">
+            {data.wines.map((wine, index) => (
+              <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+            ))}
+          </div>
+        </div>
+
+        <div className="app__specialMenu_img">
+          <img src={images.wine} alt="wine img" />
+        </div>
+
+        <div className="app__specialMenu_beers flex__center">
+          <p className='app__specialMenu_heading'>Beers</p>
+          <div className="app__specialMenu-items">
+            {data.beers.map((beer, index) => (
+              <MenuItem key={beer.title + index} title={beer.title} price={beer.price} tags={beer.tags} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div style={{marginTop: '15px'}}>
+        <button type='button' className='custom__button'>View More</button>
+      </div>
+    </div>
+  );
+}
 
 export default SpecialMenu;

90baf22376e82798fe58623cbbf76f2dfdcb555a
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-06-10 14:06:26 +0300

    change the width and height of intro

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,9 +24,6 @@ const Navbar = () => {
           <a href="#menu">Menu</a>
         </li>
         <li className="p__opensans">
-          <a href="#awards">Awards</a>
-        </li>
-        <li className="p__opensans">
           <a href="#contact">Contact</a>
         </li>
       </ul>
@@ -59,9 +56,6 @@ const Navbar = () => {
           </li>
           <li className="p__opensans">
             <a href="#menu">Menu</a>
-          </li>
-          <li className="p__opensans">
-            <a href="#awards">Awards</a>
           </li>
           <li className="p__opensans">
             <a href="#contact">Contact</a>
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Gallery.css";
 
-import './Gallery.css';
-
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+function Gallery() {
+  return(
+    <div className="app__gallery flex__center">
+      
+    </div>
+  ) 
+}
 
 export default Gallery;
